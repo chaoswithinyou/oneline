c27c57a009667bbf56b47c029fa39029fbce5be5
root <sonlam06111998@gmail.com>
2023-05-19 01:53:42 +0000

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oneline',
+    version='0.1',
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=[
+        news-please
+        pyTelegramBotAPI
+    ]
+)
