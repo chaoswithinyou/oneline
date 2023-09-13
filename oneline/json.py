2f01b52ebbe044d530792ce141f6d5da2c229464
thaikami <thaikami@github.com>
2023-09-13 08:18:46 +0700

    Add json oneliner

---
+++ oneline/json.py
@@ -0,0 +1,19 @@
+import json
+
+
+class json:
+    def __init__(self, json_path) -> None:
+        try:
+            with open(json_path) as f:
+                self.data = json.load(f)
+        except Exception as e:
+            print(e)
+
+        def __len__(self):
+            return len(self.data)
+
+        def getKeys(self):
+            return self.data.keys()
+
+        def __getitem__(self, index):
+            return self.data[index]
