8d8e744207ab1bc03a0a45a88c94a18b9a4761f4
thaikami <thaikami@github.com>
2023-09-13 08:36:39 +0700

    Update defs in jsonOps

--- oneline/json.py
+++ oneline/json.py
@@ -9,11 +9,11 @@ class jsonOps:
         except Exception as e:
             print(e)
 
-        def __len__(self):
-            return len(self.data)
+    def __len__(self):
+        return len(self.data)
 
-        def getKeys(self):
-            return self.data.keys()
+    def getKeys(self):
+        return self.data.keys()
 
-        def __getitem__(self, index):
-            return self.data[index]
+    def __getitem__(self, index):
+        return self.data[index]
